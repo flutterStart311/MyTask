85c3a2871f19004e1b509f0d8830afad839f4be2
Chamath Harshana <99314096+IT21175466@users.noreply.github.com>
2025-01-21 15:24:09 +0530

    feat(auth):SignIn Completed

--- mytodoapp_frontend/lib/features/authentication/ui/signUpScreen.dart
+++ mytodoapp_frontend/lib/features/authentication/ui/signUpScreen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mytodoapp_frontend/contants/colors.dart';
+import 'package:mytodoapp_frontend/features/authentication/bloc/auth_bloc.dart';
+import 'package:mytodoapp_frontend/features/authentication/ui/loginScreen.dart';
+import 'package:mytodoapp_frontend/models/user_model.dart';
 import 'package:mytodoapp_frontend/widgets/custom_button.dart';
 import 'package:mytodoapp_frontend/widgets/custom_textfield.dart';
 
@@ -15,6 +19,10 @@ class _SignUpScreenState extends State<SignUpScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  final AuthBloc _authBloc = AuthBloc();
+
+  bool isLoading = false;
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
@@ -22,107 +30,174 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              flex: 2,
-              child: Center(
-                child: Image.asset(
-                  'assets/images/signUpImage.png',
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 3,
-              child: Container(
-                width: screenWidth,
-                padding: EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 20,
-                ),
-                decoration: BoxDecoration(
-                  color: AppColors.signUpAccentColor,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
+      body: BlocConsumer<AuthBloc, AuthState>(
+        bloc: _authBloc,
+        listener: (context, state) {
+          if (state is SignUpInProgressState) {
+            isLoading = true;
+          } else if (state is SignUpSucessState) {
+            isLoading = false;
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(
+                  'SignUp Success',
+                  style: TextStyle(
+                    color: Colors.white,
                   ),
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'SignUp',
-                      style: TextStyle(
-                        fontSize: 36,
-                        fontWeight: FontWeight.w500,
-                        fontFamily: 'Poppins',
-                        color: AppColors.fontColorBlack,
+                backgroundColor: AppColors.accentColor,
+              ),
+            );
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => LoginScreen(),
+              ),
+            );
+          } else if (state is SignUpErrorState) {
+            isLoading = false;
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(
+                  state.error,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                backgroundColor: Colors.red,
+              ),
+            );
+          }
+        },
+        builder: (context, state) {
+          return SafeArea(
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Center(
+                    child: Image.asset(
+                      'assets/images/signUpImage.png',
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    width: screenWidth,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 20,
+                    ),
+                    decoration: BoxDecoration(
+                      color: AppColors.signUpAccentColor,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
                       ),
                     ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    CustomTextfield(
-                      controller: _nameController,
-                      lableText: 'Name',
-                      borderColor: Colors.white,
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    CustomTextfield(
-                      controller: _emailController,
-                      lableText: 'Email',
-                      borderColor: Colors.white,
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    CustomTextfield(
-                      controller: _passwordController,
-                      lableText: 'Password',
-                      borderColor: Colors.white,
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    //Button
-                    CustomButton(btnWidth: screenWidth, btnText: 'Sign Up'),
-                    SizedBox(
-                      height: 25,
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          'Already have an account?',
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w300,
-                            fontFamily: 'Poppins',
-                            color: AppColors.fontColorBlack,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'SignUp',
+                            style: TextStyle(
+                              fontSize: 36,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: 'Poppins',
+                              color: AppColors.fontColorBlack,
+                            ),
                           ),
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        Text(
-                          'Login',
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w600,
-                            fontFamily: 'Poppins',
-                            color: AppColors.accentColor,
+                          SizedBox(
+                            height: 30,
                           ),
-                        ),
-                      ],
+                          CustomTextfield(
+                            controller: _nameController,
+                            lableText: 'Name',
+                            borderColor: Colors.white,
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          CustomTextfield(
+                            controller: _emailController,
+                            lableText: 'Email',
+                            borderColor: Colors.white,
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          CustomTextfield(
+                            controller: _passwordController,
+                            lableText: 'Password',
+                            borderColor: Colors.white,
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          //Button
+                          isLoading
+                              ? SizedBox(
+                                  height: 55,
+                                  width: screenWidth,
+                                  child: Center(
+                                    child: CircularProgressIndicator(),
+                                  ),
+                                )
+                              : GestureDetector(
+                                  onTap: () {
+                                    final user = UserModel(
+                                      userID: '',
+                                      name: _nameController.text,
+                                      email: _emailController.text,
+                                      password: _passwordController.text,
+                                      fcmToken: '',
+                                    );
+
+                                    _authBloc.add(SignUpEvent(userModel: user));
+                                  },
+                                  child: CustomButton(
+                                      btnWidth: screenWidth,
+                                      btnText: 'Sign Up'),
+                                ),
+                          SizedBox(
+                            height: 25,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                'Already have an account?',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w300,
+                                  fontFamily: 'Poppins',
+                                  color: AppColors.fontColorBlack,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                'Login',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w600,
+                                  fontFamily: 'Poppins',
+                                  color: AppColors.accentColor,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
